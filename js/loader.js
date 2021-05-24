59200d85b4bbbf3188b7e9efba618a2280948b2e
Sebastian Samaniego <ssamaniego30@gmail.com>
2021-05-24 03:03:30 -0500

    cambio de clases entre sign up y login, links de js

--- js/loader.js
+++ js/loader.js
@@ -2,5 +2,5 @@ $(function(){
     $("#header").load("../includes/header.html");
     $("#footer").load("../includes/footer.html");
     $("#login").load("../includes/login.html");
-    $("#sign-up").load("../includes/Signup.html");
+    $("#signup").load("../includes/signup.html");
 });
